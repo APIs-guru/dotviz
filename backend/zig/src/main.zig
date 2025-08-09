1d8dbfb36ecae2ec7dac8ab143498c9bb1a7f9b8
vitalnodo <vitalnodo@outlook.com>
2025-08-09 15:47:51 +0300

    use local paths to speed up development and add an example

--- backend/zig/src/main.zig
+++ backend/zig/src/main.zig
@@ -4,4 +4,9 @@ const lib = @import("dotviz_lib");
 export var GVC: ?*lib.c.GVC_t = null;
 pub fn main() !void {
     _ = lib.viz_create_context();
+    const dot = "digraph {a -> b}";
+    const graphptr = lib.viz_dot_to_graph(dot);
+    _ = lib.viz_layout_graph(graphptr);
+    const svg = lib.viz_graph_to_svg(graphptr).?;
+    std.debug.print("{s}\n", .{@as([*c]u8, @ptrCast(svg))});
 }
