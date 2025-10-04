862e0b6723e1dff3a28a81161175b94ee3c372e1
vitalnodo <vitalnodo@outlook.com>
2025-10-04 18:04:06 +0300

    context_inline (2)

--- backend/src/context_inline.c
+++ backend/src/context_inline.c
@@ -1,5 +1,11 @@
+// clang-format off
+#include "alloc.h"
+#include "const.h"
 #include "gvc.h"
 #include "gvplugin.h"
+#include "gvcint.h" // IWYU pragma: keep
+#include "gvcproc.h"
+// clang-format on
 
 extern gvplugin_library_t gvplugin_core_LTX_library;
 extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
@@ -9,6 +15,32 @@ lt_symlist_t lt_preloaded_symbols[] = {
     {0, 0},
 };
 
+static char *LibInfo[] = {
+    "graphviz", /* Program */
+    "a",        /* Version */
+    "a"         /* Build Date */
+};
+
+GVC_t *my_gvNEWcontext(const lt_symlist_t *builtins, int demand_loading) {
+  GVC_t *gvc = gv_alloc(sizeof(GVC_t));
+
+  gvc->common.info = LibInfo;
+  gvc->common.errorfn = agerrorf;
+  gvc->common.builtins = builtins;
+  gvc->common.demand_loading = demand_loading;
+
+  return gvc;
+}
+
+GVC_t *my_gvContextPlugins(const lt_symlist_t *builtins, int demand_loading) {
+  GVC_t *gvc;
+
+  agattr_text(NULL, AGNODE, "label", NODENAME_ESC);
+  gvc = my_gvNEWcontext(builtins, demand_loading);
+  gvconfig(gvc, false); /* configure for available plugins */
+  return gvc;
+}
+
 GVC_t *gw_create_context(void) {
-  return gvContextPlugins(lt_preloaded_symbols, 0);
+  return my_gvContextPlugins(lt_preloaded_symbols, 0);
 }
