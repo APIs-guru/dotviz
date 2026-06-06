b0142f286f972e454c8e673ae2a360c18618c293
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2026-06-06 23:47:00 +0300

    move render_dot into Zig

--- backend/src/render_dot.zig
+++ backend/src/render_dot.zig
@@ -2,6 +2,14 @@ const vizjs_types = @import("vizjs_types.zig");
 const graphviz = vizjs_types.graphviz;
 
 pub fn renderDot(graph: ?*graphviz.Agraph_t, request: vizjs_types.RenderRequest) ?[:0]const u8 {
-    const output = graphviz.render_dot(graph, request.dotOutputMaxLineLength);
+    graphviz.my_attach_attrs_and_arrows(graph);
+
+    // reset node state
+    var n: ?*graphviz.Agnode_t = graphviz.agfstnode(graph);
+    while (n != null) : (n = graphviz.agnxtnode(graph, n)) {
+        graphviz.nodeInfoPtr(n).*.state = 0;
+    }
+
+    var output = graphviz.my_agwrite(graph, request.dotOutputMaxLineLength);
     return @ptrCast(output.data[0..output.data_position]);
 }
