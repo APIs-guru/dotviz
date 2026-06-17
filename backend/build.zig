708758e116618a5c48195a99597fb720827f2393
vitalnodo <vitalnodo@outlook.com>
2026-06-17 20:26:23 +0300

    build.zig: add list.c and arena.c to src_util (new in graphviz 15.0.0)

--- backend/build.zig
+++ backend/build.zig
@@ -511,7 +511,7 @@ const src_common = [_][]const u8{
 
 const src_util = [_][]const u8{
     //"xml.c",
-    "gv_fopen.c", "gv_find_me.c", "random.c", "base64.c",
+    "gv_fopen.c", "gv_find_me.c", "random.c", "base64.c", "list.c", "arena.c",
 };
 
 const src_pathplan = [_][]const u8{
