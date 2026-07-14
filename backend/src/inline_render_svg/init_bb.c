283f31e78bf84c01d8610abbadf07ff67563023c
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2026-07-14 03:08:38 +0300

    Localize declarations and remove unused includes

--- backend/src/inline_render_svg/init_bb.c
+++ backend/src/inline_render_svg/init_bb.c
@@ -1,26 +1,22 @@
-#include "../gv_char_classes.h"
-#include "const.h"
 #include "geomprocs.h"
 #include "gvc.h" // IWYU pragma: keep
-#include "gvcint.h"
-#include "util/list.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static boxf bezier_bb(bezier bz) {
-  pointf p, p1, p2;
-  boxf bb;
-
   assert(bz.size > 0);
   assert(bz.size % 3 == 1);
+  boxf bb;
   bb.LL = bb.UR = bz.list[0];
   for (size_t i = 1; i < bz.size;) {
     /* take mid-point between two control points for bb calculation */
-    p1 = bz.list[i];
+    pointf p1 = bz.list[i];
     i++;
-    p2 = bz.list[i];
+    pointf p2 = bz.list[i];
     i++;
+
+    pointf p;
     p.x = (p1.x + p2.x) / 2;
     p.y = (p1.y + p2.y) / 2;
     expandbp(&bb, p);
@@ -34,24 +30,21 @@ static boxf bezier_bb(bezier bz) {
 
 extern boxf arrow_bb(pointf p, pointf u, double arrowsize);
 static void init_splines_bb(splines *spl) {
-  bezier bz;
-  boxf bb, b;
-
   assert(spl->size > 0);
-  bz = spl->list[0];
-  bb = bezier_bb(bz);
+  bezier bz = spl->list[0];
+  boxf bb = bezier_bb(bz);
   for (size_t i = 0; i < spl->size; i++) {
     if (i > 0) {
       bz = spl->list[i];
-      b = bezier_bb(bz);
+      boxf b = bezier_bb(bz);
       EXPANDBB(&bb, b);
     }
     if (bz.sflag) {
-      b = arrow_bb(bz.sp, bz.list[0], 1);
+      boxf b = arrow_bb(bz.sp, bz.list[0], 1);
       EXPANDBB(&bb, b);
     }
     if (bz.eflag) {
-      b = arrow_bb(bz.ep, bz.list[bz.size - 1], 1);
+      boxf b = arrow_bb(bz.ep, bz.list[bz.size - 1], 1);
       EXPANDBB(&bb, b);
     }
   }
@@ -59,23 +52,20 @@ static void init_splines_bb(splines *spl) {
 }
 
 static void init_bb_edge(edge_t *e) {
-  splines *spl;
-
-  spl = ED_spl(e);
+  splines *spl = ED_spl(e);
   if (spl)
     init_splines_bb(spl);
 }
 
 static void init_bb_node(graph_t *g, node_t *n) {
-  edge_t *e;
-
   ND_bb(n).LL.x = ND_coord(n).x - ND_lw(n);
   ND_bb(n).LL.y = ND_coord(n).y - ND_ht(n) / 2.;
   ND_bb(n).UR.x = ND_coord(n).x + ND_rw(n);
   ND_bb(n).UR.y = ND_coord(n).y + ND_ht(n) / 2.;
 
-  for (e = agfstout(g, n); e; e = agnxtout(g, e))
+  for (edge_t *e = agfstout(g, n); e; e = agnxtout(g, e)){
     init_bb_edge(e);
+  }
 
   /* IDEA - could also save in the node the bb of the node and
   all of its outedges, then the scan time would be proportional
@@ -87,8 +77,7 @@ static void init_bb_node(graph_t *g, node_t *n) {
 }
 
 void init_bb(graph_t *g) {
-  node_t *n;
-
-  for (n = agfstnode(g); n; n = agnxtnode(g, n))
+  for (node_t *n = agfstnode(g); n; n = agnxtnode(g, n)) {
     init_bb_node(g, n);
+  }
 }
