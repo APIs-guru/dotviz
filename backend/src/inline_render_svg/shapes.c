5065c36b5fc3d33fb8898eabe9471910dd06f4ae
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2026-07-19 22:55:14 +0300

    checkStyle: Use polygon options when style is empty
    If the node's style attribute is empty, set *flagp from
    ND_shape(n)->polygon->option and return NULL. Only call parse_style
    for non-empty style strings and merge parsed flags with polygon options.

--- backend/src/inline_render_svg/shapes.c
+++ backend/src/inline_render_svg/shapes.c
@@ -439,60 +439,67 @@ static graphviz_polygon_style_t style_or(graphviz_polygon_style_t a,
 }
 
 static char **checkStyle(node_t *n, graphviz_polygon_style_t *flagp) {
-  char **pstyle = 0;
-  graphviz_polygon_style_t istyle = {0};
-
   char *style = late_nnstring(n, N_style, "");
-  if (style[0]) {
-    char **pp = pstyle = parse_style(style);
-    char *p;
-    while ((p = *pp)) {
-      if (streq(p, "filled")) {
-        istyle.filled = true;
-        pp++;
-      } else if (streq(p, "rounded")) {
-        istyle.rounded = true;
-        char **qp = pp; /* remove rounded from list passed to renderer */
-        do {
-          qp++;
-          *(qp - 1) = *qp;
-        } while (*qp);
-      } else if (streq(p, "diagonals")) {
-        istyle.diagonals = true;
-        char **qp = pp; /* remove diagonals from list passed to renderer */
-        do {
-          qp++;
-          *(qp - 1) = *qp;
-        } while (*qp);
-      } else if (streq(p, "invis")) {
-        istyle.invisible = true;
-        pp++;
-      } else if (streq(p, "radial")) {
-        istyle.radial = true;
-        istyle.filled = true;
-        char **qp = pp; /* remove radial from list passed to renderer */
-        do {
-          qp++;
-          *(qp - 1) = *qp;
-        } while (*qp);
-      } else if (streq(p, "striped") && isBox(n)) {
-        istyle.striped = true;
-        char **qp = pp; /* remove striped from list passed to renderer */
-        do {
-          qp++;
-          *(qp - 1) = *qp;
-        } while (*qp);
-      } else if (streq(p, "wedged") && isEllipse(n)) {
-        istyle.wedged = true;
-        char **qp = pp; /* remove wedged from list passed to renderer */
-        do {
-          qp++;
-          *(qp - 1) = *qp;
-        } while (*qp);
-      } else
-        pp++;
-    }
+  if (style[0] == '\0') {
+    polygon_t *poly = ND_shape(n)->polygon;
+    if (poly != NULL)
+      *flagp = poly->option;
+    else
+      *flagp = (graphviz_polygon_style_t){0};
+    return NULL;
   }
+
+  graphviz_polygon_style_t istyle = {0};
+  char **pstyle = parse_style(style);
+  char **pp = pstyle;
+  char *p;
+  while ((p = *pp)) {
+    if (streq(p, "filled")) {
+      istyle.filled = true;
+      pp++;
+    } else if (streq(p, "rounded")) {
+      istyle.rounded = true;
+      char **qp = pp; /* remove rounded from list passed to renderer */
+      do {
+        qp++;
+        *(qp - 1) = *qp;
+      } while (*qp);
+    } else if (streq(p, "diagonals")) {
+      istyle.diagonals = true;
+      char **qp = pp; /* remove diagonals from list passed to renderer */
+      do {
+        qp++;
+        *(qp - 1) = *qp;
+      } while (*qp);
+    } else if (streq(p, "invis")) {
+      istyle.invisible = true;
+      pp++;
+    } else if (streq(p, "radial")) {
+      istyle.radial = true;
+      istyle.filled = true;
+      char **qp = pp; /* remove radial from list passed to renderer */
+      do {
+        qp++;
+        *(qp - 1) = *qp;
+      } while (*qp);
+    } else if (streq(p, "striped") && isBox(n)) {
+      istyle.striped = true;
+      char **qp = pp; /* remove striped from list passed to renderer */
+      do {
+        qp++;
+        *(qp - 1) = *qp;
+      } while (*qp);
+    } else if (streq(p, "wedged") && isEllipse(n)) {
+      istyle.wedged = true;
+      char **qp = pp; /* remove wedged from list passed to renderer */
+      do {
+        qp++;
+        *(qp - 1) = *qp;
+      } while (*qp);
+    } else
+      pp++;
+  }
+
   polygon_t *poly = ND_shape(n)->polygon;
   if (poly)
     istyle = style_or(istyle, poly->option);
